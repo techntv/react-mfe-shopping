64b05fc9c605023e21f882ee1314214871d696c4
Vinh Nguyen <techntvz@gmail.com>
2022-12-23 17:17:06 +0700

    Create container deploy script

--- applications/container/config/webpack.common.js
+++ applications/container/config/webpack.common.js
@@ -1,17 +1,23 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react','@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
--- applications/container/config/webpack.dev.js
+++ applications/container/config/webpack.dev.js
@@ -1,29 +1,26 @@
 const { merge } = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
     },
-    plugins: [
-        new ModuleFederation({
-            name: 'applications',
-            remotes: {
-                'marketing': 'marketing@http://localhost:8081/remoteEntry.js'
-            },
-            shared: packageJson.dependencies
-        }),
-        new HTMLWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  },
+  plugins: [
+    new ModuleFederation({
+      name: "applications",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
